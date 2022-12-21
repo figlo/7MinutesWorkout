f20c9b366c9f4766f7b7b2bc4036d69eaf73879c
figlo <miro@figlar.sk>
2022-12-21 10:06:25 +0100

    using CountDownTimer

--- app/src/main/java/sk/figlar/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/sk/figlar/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,15 @@ package sk.figlar.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import sk.figlar.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+
+    private lateinit var restTimer: CountDownTimer
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +24,32 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExercise.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+        if (this::restTimer.isInitialized) {
+            restTimer.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding.progressBar.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Here now we will start exercise.", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
     }
 }
