d03c710bc351d19a65c44160c613a9951b6c6378
figlo <miro@figlar.sk>
2022-12-27 12:29:59 +0100

    creating recyclerview adapter and viewholder

---
+++ app/src/main/java/sk/figlar/a7minutesworkout/ExerciseStatusAdapter.kt
@@ -0,0 +1,31 @@
+package sk.figlar.a7minutesworkout
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import sk.figlar.a7minutesworkout.databinding.ItemExerciseStatusBinding
+
+class ExerciseStatusAdapter(val items: List<ExerciseModel>) :
+    RecyclerView.Adapter<ExerciseStatusAdapter.ViewHolder> {
+
+    class ViewHolder(binding: ItemExerciseStatusBinding) : RecyclerView.ViewHolder(binding.root) {
+        val tvItem = binding.tvItem
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemExerciseStatusBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val model: ExerciseModel = items[position]
+        holder.tvItem.text = model.id.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
