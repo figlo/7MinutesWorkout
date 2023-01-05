72815f5562d5788f205654d3678ad90eadac509c
figlo <miro@figlar.sk>
2023-01-05 10:33:56 +0100

    addint the room database

--- app/src/main/java/sk/figlar/a7minutesworkout/FinishActivity.kt
+++ app/src/main/java/sk/figlar/a7minutesworkout/FinishActivity.kt
@@ -1,8 +1,13 @@
 package sk.figlar.a7minutesworkout
 
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import sk.figlar.a7minutesworkout.databinding.ActivityFinishBinding
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -23,6 +28,20 @@ class FinishActivity : AppCompatActivity() {
 
         binding.btnFinish.setOnClickListener {
             finish()
+        }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(dao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+        val calendar = java.util.Calendar.getInstance()
+        val dateTime = calendar.time
+        val formatter = java.text.SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = formatter.format(dateTime)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
         }
     }
 }
---
+++ app/src/main/java/sk/figlar/a7minutesworkout/WorkOutApp.kt
@@ -0,0 +1,10 @@
+package sk.figlar.a7minutesworkout
+
+import android.app.Application
+
+class WorkOutApp: Application() {
+
+    val db by lazy {
+        HistoryDatabase.getInstance(this)
+    }
+}
